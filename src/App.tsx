9ec0bcfac8cad0c13b0074aa3a5355e9e5c25c5d
nitish <bhuvanitish@gmail.com>
2026-08-25 18:10:34 +0530

    feat: add GitHub Actions workflow for automated deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { GameStatsProvider } from './context/GameStatsContext';
 import { Header } from './components/Header';
